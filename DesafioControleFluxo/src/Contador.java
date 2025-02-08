cc584f86ff5a32a2d9a54ababa1e4addaa643a00
Pedro Reves <pedroreves11@gmail.com>
2025-02-08 17:11:52 -0300

    create CalculaDiferenca and ImprimirValores methods

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Contador {
@@ -11,6 +12,29 @@ public class Contador {
 
             System.out.println("Digite o Primeiro número");
             segundoNumero = scanner.nextInt();
+
+            int resultadoDiferenca = calculaDiferenca(primeiroNumero, segundoNumero);
+            imprimirValores(resultadoDiferenca);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println(e.getMessage());
+        } catch (InputMismatchException e) {
+            System.out.println("Não foi possível realizar operação: Erro no Input de valores");
+        }
+
+
+    }
+
+    public static int calculaDiferenca(int numero1, int numero2) throws ParametrosInvalidosException {
+        if (numero1 >= numero2) {
+            throw new ParametrosInvalidosException("Não foi possível realizar operação: Primeiro número maior que o segundo Numero!");
+        }
+
+        return numero2 - numero1;
+    }
+
+    public static void imprimirValores(int indice) {
+        for(int i = 1; i <= indice; i++) {
+            System.out.println("Imprimindo o número: " + i);
         }
     }
 }

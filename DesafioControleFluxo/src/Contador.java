8579ddfd217cb5c454b916e2ead4fa3c8f27595a
Pedro Reves <pedroreves11@gmail.com>
2025-02-08 16:41:06 -0300

    feat: receiving two numbers as args

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Contador {
     public static void main(String[] args) {
-        System.out.println("Olá Mundo");
+        int primeiroNumero;
+        int segundoNumero;
+
+        try(Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Digite o Primeiro número");
+            primeiroNumero = scanner.nextInt();
+
+            System.out.println("Digite o Primeiro número");
+            segundoNumero = scanner.nextInt();
+        }
     }
 }
